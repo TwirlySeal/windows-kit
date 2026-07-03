cb35ec2c7af6280dcf08f8b45330af201f25a729
Ethan Burnet <ethanandrewburnet@gmail.com>
2026-07-03 16:41:04 +1000

    CustomAttribute raw value parsing

---
+++ Sources/Generator/Tables/CustomAttribute.swift
@@ -0,0 +1,38 @@
+import BinaryParsing
+
+struct CustomAttribute {
+    private let metadata: MetadataDB
+    
+    private let parentIndex: CodedIndex<HasCustomAttribute.Tag>
+    private let typeIndex: CodedIndex<CustomAttributeType.Tag>
+    private let valueIndex: UInt32
+    
+    enum CustomAttributeError: Error {
+        case missingParent
+        case missingType
+    }
+    
+    private init(metadata: MetadataDB, span: inout ParserSpan) throws {
+        self.metadata = metadata
+        
+        let parentValue = try UInt32(parsingLittleEndian: &span, byteCount: Int(metadata.codedIndexSizes.hasCustomAttribute))
+        guard let parentIndex = try CodedIndex<HasCustomAttribute.Tag>(rawValue: Int(parentValue)) else {
+            throw CustomAttributeError.missingParent
+        }
+        self.parentIndex = parentIndex
+        
+        let typeValue = try UInt32(parsingLittleEndian: &span, byteCount: Int(metadata.codedIndexSizes.customAttributeType))
+        guard let typeIndex = try CodedIndex<CustomAttributeType.Tag>(rawValue: Int(typeValue)) else {
+            throw CustomAttributeError.missingType
+        }
+        self.typeIndex = typeIndex
+        
+        self.valueIndex = try UInt32(parsingLittleEndian: &span, byteCount: metadata.heapSizes.blobSize)
+    }
+    
+    init(metadata: MetadataDB, rowIndex: Index) throws {
+        self = try metadata.withRowSpan(in: .customAttribute, rowIndex: rowIndex) { span in
+            try Self(metadata: metadata, span: &span)
+        }
+    }
+}
