1c725754c46221b863c0824c94f7333dac1b1c9f
Ethan Burnet <ethanandrewburnet@gmail.com>
2026-07-14 10:45:44 +1000

    Fixed FieldSig parser not handling starting magic number

--- Sources/WinMD/Signatures/FieldSig.swift
+++ Sources/WinMD/Signatures/FieldSig.swift
@@ -1,10 +1,17 @@
 import BinaryParsing
 
+enum FieldSigError: Error {
+    case invalidMagicNumber
+}
+
 public struct FieldSig {
     public let customModifiers: [CustomMod]
     public let type: Type
     
     private init(parsing span: inout ParserSpan, in file: MetadataFile) throws {
+        guard try UInt8(parsing: &span) == 0x6 else {
+            throw FieldSigError.invalidMagicNumber
+        }
         self.customModifiers = try CustomMod.parseZeroOrMore(from: &span, in: file)
         self.type = try Type(parsing: &span, in: file)
     }
